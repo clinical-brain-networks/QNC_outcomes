1bca7cf5d234361969eee30ccad2fb997b0e5c06
ljhearne <luke.hearne@qimrberghofer.edu.au>
2023-09-04 10:46:15 +1000

    update with new data

--- code/surfice_plot.py
+++ code/surfice_plot.py
@@ -1,10 +1,14 @@
+'''
+To run these, open surfice and copy/run into the code gui
+Maybe need to update the paths appropriately.
+'''
 import gl
 
 # response plot
 gl.resetdefaults()
 gl.meshload('BrainMesh_ICBM152.lh.mz3')
 gl.nodeload(
-    '/Users/lukehearne/Documents/projects/QNC_BR/code/TMS_coords_Response.node')
+    '/Users/lukehearne/Documents/projects/QNC_BR/figures/TMS_coords_Response.node')
 gl.shaderxray(1.0, 1.0)
 gl.nodecolor('Inferno', 1)
 gl.shaderambientocclusion(.40)
@@ -15,7 +19,7 @@ gl.savebmp(
 gl.resetdefaults()
 gl.meshload('BrainMesh_ICBM152.lh.mz3')
 gl.nodeload(
-    '/Users/lukehearne/Documents/projects/QNC_BR/code/TMS_coords_Tier.node')
+    '/Users/lukehearne/Documents/projects/QNC_BR/figures/TMS_coords_Tier.node')
 gl.shaderxray(1.0, 1.0)
 gl.nodecolor('Inferno', 1)
 gl.nodethresh(0, 3)
